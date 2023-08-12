aef5679b8617c2f2daf3d7d45de8ae9aced461fd
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-08-12 14:37:36 +0300

    task number two and three

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const videoPlayer = document.querySelector('iframe')
+const player = new Player(videoPlayer);
+console.log(player)
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+}
+
+player.on('timeupdate', throttle(onPlay, 1000))
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time") || 0))
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle'
+const form = document.querySelector('.feedback-form')
 
+form.addEventListener('input', throttle(onInputFormFeedbeck, 500))
+form.addEventListener("submit", onSubmitForm)
+
+
+function onInputFormFeedbeck(event) {
+    const formValue = event.target.value
+localStorage.setItem("feedback-form-state", JSON.stringify(formValue))
+}
+
+function onSubmitForm(event) {
+    console.log(event)
+    event.preventDefault()
+
+    const { email, message } = event.currentTarget.elements
+    console.log({ email: email.value, message : message.value})
+   
+    event.currentTarget.reset()
+    localStorage.removeItem("feedback-form-state")
+}
