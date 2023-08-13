ac20c38326340a2d724224c5598828f3519ead46
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-08-13 12:31:22 +0300

    task number three

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,84 @@
 import throttle from 'lodash.throttle'
 const form = document.querySelector('.feedback-form')
+let KeyLocale = JSON.parse(localStorage.getItem("feedback-form-state")) || {}
+
 
 form.addEventListener('input', throttle(onInputFormFeedbeck, 500))
 form.addEventListener("submit", onSubmitForm)
+checkАieldКestart()
 
 
 function onInputFormFeedbeck(event) {
-    const formValue = event.target.value
-localStorage.setItem("feedback-form-state", JSON.stringify(formValue))
-}
+    KeyLocale[event.target.name] = event.target.value
+    // const data = {email: form.elements.email.value, message: form.elements.message.value } 
+    // console.log(data)
+    localStorage.setItem("feedback-form-state", JSON.stringify(KeyLocale))
+} 
+
 
 function onSubmitForm(event) {
-    console.log(event)
     event.preventDefault()
 
-    const { email, message } = event.currentTarget.elements
-    console.log({ email: email.value, message : message.value})
-   
-    event.currentTarget.reset()
+    const {email , message} = form.elements
+    console.log({ email: email.value, message: message.value })
+    
     localStorage.removeItem("feedback-form-state")
+    event.currentTarget.reset()
 }
+
+function checkАieldКestart() {
+    if (KeyLocale !== '') {
+        const { email, message } = form.elements
+        email.value = KeyLocale.email || ''
+        message.value = KeyLocale.message || ''
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// checkValue()
+
+// function onInputFormFeedbeck(event) {
+//     const formValue = event.target.value
+// localStorage.setItem("feedback-form-state", JSON.stringify(formValue))
+// }
+
+// function onSubmitForm(event) {
+//     console.log(event)
+//     event.preventDefault()
+
+//     const { email, message } = event.currentTarget.elements
+//     console.log({ email: email.value, message : message.value})
+   
+//     event.currentTarget.reset()
+//     localStorage.removeItem("feedback-form-state")
+// }
+
+// function checkValue() {
+//     if (foo) {
+//         let { email, message } = form.elements
+        
+//         email.value = foo.email || ''
+//         message.value = foo.message  || ''
+//     } 
+// }
